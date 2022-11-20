50c1130b06dc9322286ab7c07e1b93141c6e7457
Kelly <kellygonza12@gmail.com>
2022-11-19 18:13:42 -0800

    Finished main DOM layout

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let userScore = parseInt(0);
-let computerScore = parseInt(0);
+let userScore = 0;
+let computerScore = 0;
 
 function playerSelection(player){
     //if player chooses 'rock' will return a string
-    if ( player=== 'rock') {
+    if ( player=== 'Rock') {
         return 'Player has chosen: Rock!';
         // else if player chooses 'paper' it will return a string stating that
-    } else if( player === 'paper') {
+    } else if( player === 'Paper') {
         return 'Player has chosen: Paper!'
         // else if player has chosen 'scissors' it will return a string
-    } else if( player === 'scissors') {
+    } else if( player === 'Scissors') {
         return 'Player has chosen: Scissors!'
         // else; if player has not chosen any of the above statements, it will return an error
     } else {
@@ -19,7 +19,7 @@ function playerSelection(player){
 
 
 function computerSelection(){
-    const choices = ['scissors', 'rock', 'paper'];
+    const choices = ['Scissors', 'Rock', 'Paper'];
 
     let choice = choices[Math.floor(Math.random() * choices.length)];
 
@@ -36,9 +36,9 @@ function singleGame(p1, p2){
 
     //player 1 is scissors
 
-    if (p1 === 'scissors') {
+    if (p1 === 'Scissors') {
         // if player 2 is paper
-        if (p2 === 'paper') {
+        if (p2 === 'Paper') {
             //player 2 will lose
             userScore++;
             return 'Player 1 has won: Scissors beat paper';
@@ -49,10 +49,10 @@ function singleGame(p1, p2){
             return 'Computer has won: Rock Beats Scissors';
         }
     // player 1 is Rock
-    } else if (p1 === 'rock') {
+    } else if (p1 === 'Rock') {
         // if player 2 is scissors
         // player 2 loses
-        if(p2 === 'scissors') {
+        if(p2 === 'Scissors') {
             userScore++;
             return 'Player 1 has won: Rock beats Scissors';
         } else{
@@ -63,10 +63,10 @@ function singleGame(p1, p2){
         }
     }else {
             // Player 1 is Paper
-        if (p1 === 'paper') {
+        if (p1 === 'Paper') {
         // if player 2 is rock
         // player 1 wins
-            if (p2 === 'rock') {
+            if (p2 === 'Rock') {
                 userScore++;
                 return 'Player 1 has won: Paper beats Rock';
             } else {
@@ -80,20 +80,30 @@ function singleGame(p1, p2){
 
 }
 
+function updateScore(){
+	const cScore = document.querySelector('.computerScore');
+	const pScore = document.querySelector('.playerScore');
+	pScore.textContent = `Your score = ${userScore}`;
+	cScore.textContent =`Computer's score = ${computerScore}`;
+};
 
+function checkWinner() {
+	let pScore = userScore;
+	let cScore = computerScore;
 
-
-// function playGame() {
-// // deleted prompt. instead we will add the clickable buttons choices
-// 	const player = this.textContent
-//     console.log(playerSelection())
-//     computer = computerSelection();
-//     console.log(`Computer has chosen: ${computer}!`)
-//     console.log(singleGame(player, computer));
-//     console.log(`Your score = ${userScore}`)
-//     console.log(`Computer's score = ${computerScore}`)
-// }
-// console.log(playGame())
+	if (pScore === 5 || cScore === 5){
+		if (pScore === 5){
+			let winner = 'You have Won. Congrats!';
+			alert(winner);
+			return true;
+		}else{
+			let winner = 'Computer has won. Better luck next time.'
+			alert(winner);
+			return true;
+		}
+	}
+	return false;
+}
 
 // attach event listener 'click' to a group of buttons
 const buttons = document.querySelectorAll('button');
@@ -103,14 +113,26 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
 	button.addEventListener('click', function playGame() {
 // deleted prompt. instead we will add the clickable buttons choices
+		const sGame = document.querySelector('.singleGame');
+		const pChoice = document.querySelector('.playerChoice');
+		const cChoice = document.querySelector('.computerChoice');
 		const player = this.textContent;
-    	console.log(playerSelection(player))
+    	
+		pChoice.textContent = playerSelection(player)
     	computer = computerSelection();
-    	console.log(`Computer has chosen: ${computer}!`)
-    	console.log(singleGame(player, computer));
-    	console.log(`Your score = ${userScore}`)
-    	console.log(`Computer's score = ${computerScore}`)
+		cChoice.textContent = `Computer has chosen: ${computer}!`
+
+		sGame.textContent = singleGame(player, computer);
+
+		updateScore();
+		
+		if(checkWinner()) {
+			userScore = 0;
+			computerScore = 0;
+			updateScore();
+		}
 	});
 });
+
 
 
